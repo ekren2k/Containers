e02a1f16427b90256e0dbbf8024662275869e144
ekren <ekrenofficial@gmail.com>
2025-04-18 11:20:36 +0200

    Implemented GetAllContainers

--- src/Containers.API/Program.cs
+++ src/Containers.API/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("UniversityDatabase");
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- src/Containers.Application/ContainerService.cs
+++ src/Containers.Application/ContainerService.cs
@@ -1,6 +1,51 @@
-﻿namespace Containers.Application;
+﻿
+using Containers.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Containers.Application;
 
 public class ContainerService
 {
+    
+    private string connectionString;
+
+    public ContainerService(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+    public IEnumerable<Container> GetAllContainers()
+    {
+        List<Container> containers = new List<Container>();
+        const string queryString = "SELECT * FROM Containers";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand(queryString, connection);
+            
+            connection.Open();
+            
+            SqlDataReader reader = command.ExecuteReader();
+
+            try
+            {
+                if (reader.HasRows)
+                {
+                    var containerRow = new Container()
+                    {
+                        ID = reader.GetInt32(0),
+                        ContainerTypeId = reader.GetInt32(1),
+                        IsHazardous = reader.GetBoolean(2),
+                        Name = reader.GetString(3),
+                    };
+                    containers.Add(containerRow);
+                }
+            }
+            finally
+            {
+                reader.Close();
+            }
+        }
+        return containers;
+    }
     
 }
